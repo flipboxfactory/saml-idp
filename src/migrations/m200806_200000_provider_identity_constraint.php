ff59610ac05ce0ce57e52ed1a6eb34a57a727aed
Damien Smrt <damien@flipboxdigital.com>
2020-10-29 17:55:14 -0600

    Catching the exception if the table doesnt need to modified.

--- src/migrations/m200806_200000_provider_identity_constraint.php
+++ src/migrations/m200806_200000_provider_identity_constraint.php
@@ -4,6 +4,7 @@ namespace flipbox\saml\idp\migrations;
 
 use flipbox\saml\core\migrations\m200806_200000_provider_identity_constraint as abstractMigration;
 use flipbox\saml\idp\records\ProviderIdentityRecord;
+use flipbox\saml\idp\Saml;
 
 /**
  */
@@ -12,5 +13,15 @@ class m200806_200000_provider_identity_constraint extends abstractMigration
 
     protected static function getIdentityTableName() {
         return ProviderIdentityRecord::tableName();
+    }
+
+    public function safeUp()
+    {
+        try{
+            // might not need this ... move on, on exception
+            parent::safeUp();
+        }catch(\Exception $e) {
+            Saml::warning($e->getMessage() . ' -> ' . $e->getTraceAsString());
+        }
     }
 }
