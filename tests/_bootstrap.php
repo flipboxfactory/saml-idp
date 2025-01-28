0bea7bb4674bd58f1500c4683370a7f58fdc61e0
Damien Smrt <dsmrt86@gmail.com>
2025-01-27 18:17:40 -0700

    fix: tests

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,9 +1,12 @@
 <?php
+
 use craft\test\TestSetup;
+
 ini_set('date.timezone', 'UTC');
 date_default_timezone_set('UTC');
 // Use the current installation of Craft
 define('CRAFT_TESTS_PATH', __DIR__);
+define('CRAFT_ROOT_PATH', __DIR__);
 define('CRAFT_STORAGE_PATH', __DIR__ . DIRECTORY_SEPARATOR . '_craft' . DIRECTORY_SEPARATOR . 'storage');
 define('CRAFT_TEMPLATES_PATH', __DIR__ . DIRECTORY_SEPARATOR . '_craft' . DIRECTORY_SEPARATOR . 'templates');
 define('CRAFT_CONFIG_PATH', __DIR__ . DIRECTORY_SEPARATOR . '_craft' . DIRECTORY_SEPARATOR . 'config');
@@ -12,4 +15,3 @@ define('CRAFT_TRANSLATIONS_PATH', __DIR__ . DIRECTORY_SEPARATOR . '_craft' . DIR
 define('CRAFT_VENDOR_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor');
 $devMode = true;
 TestSetup::configureCraft();
-
